9f2728fd033f51e950acc6d1a2cb7bab958135c0
Alex Grande <grandecomplex@gmail.com>
2012-08-22 14:28:50 -0700

    Now works well just needs work on the hiding or closing of list

--- javascripts/tag.js
+++ javascripts/tag.js
@@ -1,14 +1,42 @@
 
 (function() {
-  
-  
   var SEPARATOR = ",";
   
   var currentIndex = -1;
   
-  var listLength;
+  var listItemLength;
+  
+  var isListShowing = false;
+  
+  var LIST_ITEM_HEIGHT;
+  
+  var ITEMS_VISIBLE_COUNT;
+  
+  var listScrollPosition = 0;
+  
+  var maxVisibleItem, minVisibleItem = 0;
+  
+  function setHeight($parent, $items, numberOfItems) {
+    var height; 
+    
+    if (!numberOfItems) {
+      numberOfItems = 5;
+    }
+    
+    ITEMS_VISIBLE_COUNT = numberOfItems;
+    resetMaxMinVisibleItems();
+    
+    LIST_ITEM_HEIGHT = $items.outerHeight();
+    
+    $parent.height(LIST_ITEM_HEIGHT*numberOfItems);
+  }
+  
+  function resetMaxMinVisibleItems() {
+    maxVisibleItem = ITEMS_VISIBLE_COUNT;
+    minVisibleItem = 0;
+  }
 
-  var Tag = function(namespace) {
+  var Tag = function(namespace, options) {
     namespace = "#"+namespace+"-";
 
     this.$inputField = $(namespace+"text");
@@ -18,9 +46,11 @@
     
     this.$items = this.$list.find("li");
     
-    listLength = this.$items.length;
+    setHeight(this.$list, this.$items);
+    
+    listItemLength = this.$items.length;
 
-    this.$inputField.quicksearch(namespace+"list li");
+    this.quicksearch = this.$inputField.quicksearch(namespace+"list li");
 
     this.addEvents();
   };
@@ -28,29 +58,69 @@
   Tag.prototype.addEvents = function() {
     var that = this;
 
-    this.$list.find("li").click(function() {
+    this.$list.find("li").mousedown(function() {
       var text = $(this).text();
       that.addTag(text);
     });
+    
+    this.$list.keydown(function(e) {
+      e.preventDefault();
+    });
+    
+    this.$inputField.keyup(function() {
+      var lastItem;
+      if (that.quicksearch.matchedResultsCount === 1) {
+        lastItem = that.$list.find("li:visible");
+        lastItem.addClass("highlighted");
+        currentIndex = lastItem.index();
+      }
+      
+      that.$list.find(".highlighted:hidden").removeClass("highlighted");
+    });
+    
+    this.$inputField.blur(function() {
+      that.hideList();
+    });
 
     this.$inputField.focus(function() {
-      this.$list.addClass("showing-list");
-    }.bind(this))
+      that.showList();
+    })
     .keydown(function (e){
       var keyCode = e.keyCode;
-
-      that.keyAction(keyCode);
+      that.keyAction(e);
 
       if(keyCode === 13) {
         e.preventDefault();
+        var highlighted = that.$list.find(".highlighted");
+        if (highlighted.length) {
+          that.addTag(highlighted.text());
+        }
         this.blur();
       }
+    });
+    
+    this.$items.live("hover", function() {
+      that.$items.eq(currentIndex).removeClass("highlighted");
+      currentIndex = $(this).index();
+      $(this).addClass("highlighted");
     });
   };
 
   Tag.prototype.hideList = function() {
     this.$list.removeClass("showing-list");
-
+    isListShowing = false;
+    currentIndex = -1;
+    resetMaxMinVisibleItems();
+    listScrollPosition = 0;
+  };
+  
+  Tag.prototype.showList = function() {
+    this.$list.addClass("showing-list");
+    this.$list.find("li").css("display", "block");
+    isListShowing = true;
+    currentIndex = -1;
+    this.$list.find(".highlighted").removeClass("highlighted");
+    this.$list.scrollTop(0);
   };
 
   Tag.prototype.addTag = function(text) {
@@ -93,64 +163,97 @@
   };
 
   Tag.prototype.needsScroll = function() {
-
-  };
-
-  Tag.prototype.scrollToNextItem = function() {
-    if (listLength === currentIndex) {
-      return
+    console.log("maxVisibileItem", maxVisibleItem)
+    if (currentIndex+1 > maxVisibleItem || currentIndex < minVisibleItem) {
+      console.log("needs scroll")
+      return true;
     }
+    console.log("Doesnt need scroll")
+    return false;
+  };
+  
+  Tag.prototype.scroll = function (direction) {
+    var scrollAmount;
+    
+    if (direction === "up") {
+      maxVisibleItem--;
+      minVisibleItem--;
+      scrollAmount = -LIST_ITEM_HEIGHT
+    } else {
+      maxVisibleItem++;
+      minVisibleItem++;
+      scrollAmount = LIST_ITEM_HEIGHT
+    }
+    
+    listScrollPosition += scrollAmount;
+        
+    this.$list.scrollTop(listScrollPosition);
   };
 
   Tag.prototype.selectItem = function(direction) {
+    if ( !isListShowing && direction !== "down" && direction !== "up" ) {
+      return;
+    }
+    
+    this.$items.eq(currentIndex).removeClass("highlighted");
+    
     if (direction === "down") {
-      if (listLength === currentIndex) {
+      if (listItemLength <= currentIndex+1) {
+        this.$items.eq(currentIndex).addClass("highlighted");
+        
         return;
       }
       
-      this.$items.eq(currentIndex).removeClass("highlighted");
-      currentIndex++
+      currentIndex++;
       this.$items.eq(currentIndex).addClass("highlighted");
       
     } else if (direction === "up") {
-      if (currentIndex === 0) {
-        this.hideList();
-      } else {
-        
-        this.$items.eq(currentIndex).removeClass("highlighted");
-        currentIndex--
+      
+      if (currentIndex > 0) {
+
+        currentIndex--;
         this.$items.eq(currentIndex).addClass("highlighted");
         
+      } else if (currentIndex === 0) {
+        currentIndex = -1;
       }
     }
+    
+    if (this.needsScroll()) {
+      this.scroll(direction);
+    }
+    
+    console.log(currentIndex)
   };
 
-  Tag.prototype.keyAction = function(key) {
-    switch(key) {
+  Tag.prototype.keyAction = function(e) {
+    switch(e.keyCode) {
       case 40:
+        if (!isListShowing) {
+          this.showList();
+        }
         this.selectItem("down");
         break;
-      case 39:
-        this.addTag();
+      case 27:
+        this.hideList();
         break;
       case 38:
         this.selectItem("up");
       break;
       case 13:
-        this.addTag();
+        this.addTag(e.target.value);
         break;
       case 9:
         this.hideList();
         break;
     }
   };
+  
 
   if (typeof define !== "undefined") {
     define("Tag", [], Tag);
   } else {
     window.Tag = Tag;
   }
-
-
 
 })();
